5d86f64105619893407e231d91de35297eb084f3
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-08 17:33:32 -0500

    adding all files

--- tail.js
+++ tail.js
@@ -11,8 +11,10 @@ const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
+
 assertEqual(tail([2,14,46,22]),[14,46,22]);
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
