880ca110b9ed700ade8231e2376154cf76c82af5
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-08 19:27:13 -0500

    adding without.js

---
+++ without.js
@@ -0,0 +1,35 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(array1, array2) {
+ 
+  if (eqArrays(array1,array2)) {
+    console.log(`🌼Assertion Passed ${array1} === ${array2}`);
+  
+  } else {
+    console.log(`🌚Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let arrayRemoved = source;
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      source[i] === itemsToRemove[j] ? arrayRemoved.splice(i,1) : "";
+    }
+    
+  }
+  return arrayRemoved;
+ 
+
+
+};
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+assertArrayEqual([1,2], without([1, 2, 3], [1]));
