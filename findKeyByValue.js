4624873314cd2818288d67e3b0fec0d9008606ac
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-09 16:41:03 -0500

    adding findKeyByValue,js

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌼Assertion Passed ${actual} === ${expected}`);
+  
+  } else {
+    console.log(`🌚Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(showObject, show) {
+  let showName;
+  for (const key in showObject) {
+    showName = showObject[key];
+    if (showName === show) {
+      return key;
+    }
+  }
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
