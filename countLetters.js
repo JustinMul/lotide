63f892c4efd8efd4fdfbb4cfcbcbb798293d6ccc
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-09 15:23:02 -0500

    adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌼Assertion Passed ${actual} === ${expected}`);
+  
+  } else {
+    console.log(`🌚Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let removeSpaceSentence = sentence.replaceAll(' ','');
+  let letterCounter = {};
+
+  for (const letter of removeSpaceSentence) {
+    if (letterCounter[letter]) {
+      letterCounter[letter]++;
+    } else {
+      letterCounter[letter] = 1;
+    }
+  }
+  return letterCounter;
+};
+
+console.log(countLetters("lighthouse in the house"));
