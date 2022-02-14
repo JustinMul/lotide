3ce1ec394054d78b2abfc56bf6e1966a6187c78a
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-14 10:23:24 -0500

    adding refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🌼Assertion Passed ${actual} === ${expected}`);
-  
-  } else {
-    console.log(`🌚Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     console.log(array1[i],array2[i]);
@@ -17,13 +8,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3,4]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
-
-const names = ['Graham'];
-names = [];
-console.log(names)
+module.exports = eqArrays;
