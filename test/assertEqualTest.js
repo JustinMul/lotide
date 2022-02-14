fa3ac741c73b5a404cdbbccc957226de5d26eecf
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-14 09:29:34 -0500

    adding refactored head and assertEqual files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual.js');
+
+assertEqual('Lighthouse Labs','Bootcamp');
+assertEqual('hi','Hi');
+assertEqual('Hi','Hi');
+assertEqual(3,2);
+assertEqual(3,3);
+
