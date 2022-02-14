08b95693dc6948fc419752127ddfc9df9feb612a
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-14 15:16:58 -0500

    adding index and a fix to find key

---
+++ index.js
@@ -0,0 +1,45 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual.js');
+const assertEqual = require('./assertEqual.js');
+const assertObjectsEqual = require('./assertObjectsEqual.js');
+const countLetters = require('./countLetters.js');
+const countOnly = require('./countOnly.js');
+const eqArrays = require('./eqArrays.js');
+const eqObjects = require('./eqObjects.js');
+const findKey = require('./findKey.js');
+const findKeyByValue = require('./findKeyByValue.js');
+const flatten = require('./flatten.js');
+const index = require('./index.js');
+const letterPositions = require('./letterPositions.js');
+const map = require('./map.js');
+const takeUntil = require('./takeUntil.js');
+const without = require('./without.js');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey:findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  index: index,
+  letterPositions: letterPositions,
+  map:map,
+  takeUntil: takeUntil,
+  without:without
+
+  
+
+
+  
+};
